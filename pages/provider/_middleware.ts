724371000ee36472b718dc897eb414614e59254c
Francis James Tolentino <francistolentino1107@gmail.com>
2022-02-11 22:46:33 +0800

    middleware for provider

--- pages/provider/_middleware.ts
+++ pages/provider/_middleware.ts
@@ -1,3 +1,16 @@
+
+
+/*
+
+Multi Service Platform - Provider Login Middleware route
+Created: Feb. 11, 2022
+Last Updated: Feb. 11, 2022
+Author: Tolentino, Francis James S.
+
+*/
+
+
+
 import { NextRequest, NextResponse } from "next/server";
 
 
@@ -5,7 +18,10 @@ import { NextRequest, NextResponse } from "next/server";
 export const middleware = (req: NextRequest) => {
     const path = req.nextUrl.pathname;
 
-    if (req.cookies.user && path === 'provider/login'){
+    console.log(req.cookies.user);
+
+    if (req.cookies.user && path === '/provider/login'){
+        console.log('redirect na dapat');
         return NextResponse.redirect(new URL('/provider', req.url));
     }
 
