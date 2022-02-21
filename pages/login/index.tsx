82b5fff812935221d33802990819a1bfcafdfe50
kiks12 <francistolentino1107@gmail.com>
2022-02-21 16:07:34 +0800

    removed unnecessary codes in login

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -3,7 +3,7 @@
 
 Multi Service Platform (User Web) - Login Page
 Created: Feb. 07, 2022
-Last Updated: Feb. 18, 2022
+Last Updated: Feb. 21, 2022
 Author: Tolentino, Francis James S.
 
 */
@@ -35,12 +35,7 @@ import Logo from '../../src/components/LoginRegister/Logo';
 const Login : NextPage = () => {
 
 
-    // const [email, setEmail] = useState<string>('');
-    // const [password, setPassword] = useState<string>('');
-
-
     const { message, loginWithGoogle, loginWithFacebook } = useAuthentication();
-
 
 
 
@@ -82,79 +77,6 @@ const Login : NextPage = () => {
                                 </div>
                             )
                         }
-                
-
-                        {/* <form 
-                            className='login-register-form'
-                        >
-
-                            <div
-                                style={{
-                                    flex: '1'
-                                }}
-                            >
-                                <div>
-                                    <label>Email</label>
-                                    <input 
-                                        name='email'
-                                        type='email'
-                                        placeholder='example@gmail.com'
-                                        className='form-control'
-                                        value={email}
-                                        onChange={(e) => setEmail(e.target.value)}
-                                    />
-                                </div>
-
-
-                                <div 
-                                    style={{
-                                        margin: '1em 0 0 0'
-                                    }}
-                                >
-                                    <label>Password</label>
-                                    <div
-                                        style={{
-                                            display: 'flex',
-                                            alignItems: 'center'
-                                        }}
-                                    >
-                                        <input 
-                                            name='password'
-                                            type='password'
-                                            placeholder='Enter your password'
-                                            className='form-control'
-                                            value={password}
-                                            onChange={(e) => setPassword(e.target.value)}
-                                        />
-                                        <div
-                                            style={{
-                                                height: '100%',
-                                                padding: '0.2em',
-                                                border: '0.3px solid var(--secondaryPurple)',
-                                                backgroundColor: 'var(--secondaryPurple)',
-                                                color: 'var(--white)',
-                                                cursor: 'pointer'
-                                            }}
-                                        >
-                                            <small>see</small>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div>
-                                <button 
-                                    className='button main-button'
-                                    style={{
-                                        margin: '0 0 3em 0'
-                                    }}
-                                    type='submit'
-                                >
-                                    Login
-                                </button>
-                            </div>
-
-                        </form> */}
 
 
 
