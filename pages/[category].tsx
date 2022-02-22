638a387c3c6a1efeaf1b5a83f78d861744e39f25
Francis James Tolentino <francistolentino1107@gmail.com>
2022-02-23 00:03:53 +0800

    category fetching to backend API working

--- pages/[category].tsx
+++ pages/[category].tsx
@@ -1,18 +1,113 @@
 
+/*
+
+Multi Service Platform - Category Page
+Created: Feb. 22, 2022
+Last Updated: Feb. 22, 2022
+Author: Tolentino, Francis James S.
+
+*/
 
 
 
-import { NextPage } from "next";
+
+import { GetServerSideProps, GetServerSidePropsContext, InferGetServerSidePropsType, NextPage } from "next";
 
 
 
-const CategoryPage: NextPage = () => {
+import { Key, useEffect } from "react";
+import { useAuthentication } from "../src/custom-hooks/useAuthentication";
+
+
+
+import fetchUserInformation from "../libs/fetchUserInformation";
+
+
+
+import Layout from "../src/components/layout/Layout";
+import authorizedFetch from "../utils/authorizedFetch";
+import { __backend__ } from "../src/constants";
+
+
+
+
+const CategoryPage: NextPage = ({
+    user,
+    services}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+
+    const { setSession } = useAuthentication();
+
+
+
+    useEffect(() => {
+        if (typeof setSession === 'function') setSession(user);
+    }, [setSession, user]);
+
+
 
     return (
-        <div>
-            <p>adfassfdsf</p>
-        </div>
+        <Layout>
+            {
+                services.length !== 0 && services.map((service: any, idx: Key | null | undefined) => {
+                    return (
+                        <p key={idx}>{JSON.stringify(service)}</p>
+                    )
+                })
+            }
+        </Layout>
     )    
+}
+
+
+
+
+export const getServerSideProps: GetServerSideProps = async ({
+    req, 
+    query}: GetServerSidePropsContext) => {
+
+
+    const category = query.category;
+
+
+    const res = await authorizedFetch({
+        url: `${__backend__}/services?category=${category}`,
+        method: 'GET',
+        accessToken: req.cookies?.accessToken,
+    })
+
+
+    
+    if (req.cookies.accessToken) {
+        const userInformation = await fetchUserInformation(req.cookies?.accessToken);
+
+
+        if (!userInformation) {
+            return {
+                props: {
+                    user: {},
+                    services: res.services,
+                }
+            }
+        }
+
+
+
+        return {
+            props: {
+                user: userInformation.user,
+                services: res.services,
+            }
+        }
+    }
+
+
+
+    return {
+        props: {
+            user: {},
+            services: res.services,
+        }
+    }
 }
 
 
