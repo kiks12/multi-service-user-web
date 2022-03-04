c319792e67530c33842b771d84b0148080e310f5
Francis James Tolentino <francistolentino1107@gmail.com>
2022-03-04 14:09:59 +0800

    responsive top navbar

--- src/components/NavigationBars/TopNavbar.tsx
+++ src/components/NavigationBars/TopNavbar.tsx
@@ -4,7 +4,7 @@
 
 Multi Service Platform - Top Navigation Bar Component
 Created: Feb. 09, 2022
-Last Updated: Mar. 01, 2022
+Last Updated: Mar. 04, 2022
 Author: Tolentino, Francis James S.
 
 */
@@ -19,6 +19,16 @@ import React, { useState } from 'react';
 import AccountButton from './AccountButton';
 import TopNavbarPopUp from './TopNavbarPopUp';
 
+
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+    faMessage,
+} from '@fortawesome/free-regular-svg-icons';
+import {
+    faMagnifyingGlass,
+    faBars
+} from '@fortawesome/free-solid-svg-icons';
 
 
 interface TopNavbarProps {
@@ -36,7 +46,12 @@ const TopNavbar : React.FC <TopNavbarProps> = ({ accessToken }) => {
     return (
         <div className='top-navbar'>
 
-            <div>
+            <div className='top-navbar-logo'>
+                <FontAwesomeIcon 
+                    icon={faBars} 
+                    size='lg'
+                    className='top-navbar-menu'
+                />
                 <h1>LOGO</h1>
             </div>
 
@@ -56,7 +71,12 @@ const TopNavbar : React.FC <TopNavbarProps> = ({ accessToken }) => {
                             type='submit'
                             className='button'
                             >
-                            Search
+                            <FontAwesomeIcon 
+                                icon={faMagnifyingGlass}
+                                style={{
+                                    color: 'var(--secondaryPurple)',
+                                }}
+                            />
                         </button>
                     </div>
                 </form>
@@ -65,10 +85,17 @@ const TopNavbar : React.FC <TopNavbarProps> = ({ accessToken }) => {
 
             <div className='top-navbar-ul-container'>
                 <ul className='top-navbar-ul'>
-                    <li className='top-navbar-li'>Messages</li>
+                    <li className='top-navbar-li'>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} size='lg'/>
+                    </li>
+                    <li className='top-navbar-li'>
+                        <FontAwesomeIcon icon={faMessage} size='lg' />
+                    </li>
 
                     <Link href='/provider' passHref={true} >
-                        <li className='top-navbar-li'>My Shop</li>
+                        <li className='top-navbar-li'>
+                            <p>My Shop</p>
+                        </li>
                     </Link>
                     
                     <li 
