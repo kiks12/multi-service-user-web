d44d5c60d6d1df6413e9e2f9e973a045a4711d29
Francis James Tolentino <francistolentino1107@gmail.com>
2022-03-04 16:57:05 +0800

    better service page menu bar responsiveness

--- src/components/ServicePage/MenuBar/index.tsx
+++ src/components/ServicePage/MenuBar/index.tsx
@@ -42,13 +42,15 @@ const MenuBar: React.FC = () => {
         <div
             className={styles.menuContainer}
         >
-            <ul className={styles.menuUl}>
-                <li className={activePart === 'Overview' ? 'menu-li-active' : 'menu-li'}>Overview</li>
-                <li className={activePart === 'Description' ? 'menu-li-active' : 'menu-li'}>Description</li>
-                <li className={activePart === 'AboutProvider' ? 'menu-li-active' : 'menu-li'}>About the Provider</li>
-                <li className={activePart === 'Reviews' ? 'menu-li-active' : 'menu-li'}>Reviews</li>
-                <li className={activePart === 'FAQs' ? 'menu-li-active' : 'menu-li'}>FAQs</li>
-            </ul>
+            <div className={styles.menuScrollWrapper}>
+                <ul className={styles.menuUl}>
+                    <li className={activePart === 'Overview' ? 'menu-li-active' : 'menu-li'}>Overview</li>
+                    <li className={activePart === 'Description' ? 'menu-li-active' : 'menu-li'}>Description</li>
+                    <li className={activePart === 'AboutProvider' ? 'menu-li-active' : 'menu-li'}>About the Provider</li>
+                    <li className={activePart === 'Reviews' ? 'menu-li-active' : 'menu-li'}>Reviews</li>
+                    <li className={activePart === 'FAQs' ? 'menu-li-active' : 'menu-li'}>FAQs</li>
+                </ul>
+            </div>
 
 
             <div className={styles.controlsContainer}>
