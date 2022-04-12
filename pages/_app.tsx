a69895f8fefd56febab095ac6232212c990299f3
Francis James Tolentino <francistolentino1107@gmail.com>
2022-04-12 23:26:30 +0800

    Web Socket Provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -25,7 +25,7 @@ import React from 'react';
 
 
 import { AuthProvider } from '../src/custom-hooks/useAuthentication';
-// import { WebSocketProvider } from '../src/custom-hooks/useWebSocket';
+import { WebSocketProvider } from '../src/custom-hooks/useWebSocket';
 
 
 
@@ -45,11 +45,11 @@ const MyApp = ({ Component, pageProps: { session, ...pageProps } }: AppProps) =>
             <Head>
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
             </Head>
-            {/* <WebSocketProvider> */}
+            <WebSocketProvider>
                 <AuthProvider>
                     <Component {...pageProps}/>
                 </AuthProvider>
-            {/* </WebSocketProvider> */}
+            </WebSocketProvider>
         </>
     )
 }
