d6fb5bd6ca65e04da574c7fae1fff124fa532c20
Francis James Tolentino <francistolentino1107@gmail.com>
2022-04-14 18:02:50 +0800

    API URLs

--- src/constants.ts
+++ src/constants.ts
@@ -5,3 +5,4 @@ export const __backend__ = process.env.BACKEND_URL;
 
 export const CREATE_CONVERSATION_API = `${__backend__}/conversation/create-conversation`;
 export const GET_CONVERSATION_DETAILS_API = `${__backend__}/conversation/get-conversation-details`;
+export const GET_LIST_OF_CONVERSATIONS_API = `${__backend__}/conversation/get-list-of-conversations`;
