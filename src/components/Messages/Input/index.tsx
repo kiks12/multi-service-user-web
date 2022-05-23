e9316fb24cb8e13b1c4799c651ffe866db2befb9
Francis James Tolentino <francistolentino1107@gmail.com>
2022-05-23 14:11:22 +0800

    offset height every send of message

--- src/components/Messages/Input/index.tsx
+++ src/components/Messages/Input/index.tsx
@@ -1,76 +1,73 @@
+import { faCirclePlus } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { useEffect, useState } from "react";
+import { useAuthentication } from "../../../custom-hooks/useAuthentication";
+import { useConversation } from "../../../custom-hooks/useConversation";
+import { useMessages } from "../../../custom-hooks/useMessages";
+import useWebSocket from "../../../custom-hooks/useWebSocket";
 
-import { faCirclePlus } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React, { useEffect, useState } from 'react';
-import { useAuthentication } from '../../../custom-hooks/useAuthentication';
-import { useConversation } from '../../../custom-hooks/useConversation';
-import { useMessages } from '../../../custom-hooks/useMessages';
-import useWebSocket from '../../../custom-hooks/useWebSocket';
-
-
-
-import styles from './Input.module.css';
-
+import styles from "./Input.module.css";
 
 interface MessagesInputProps {
     accessToken: string;
+    containerRef: React.RefObject<HTMLDivElement>;
+    secondContainerRef: React.RefObject<HTMLDivElement>;
 }
 
-
-
-const MessagesInput : React.FC<MessagesInputProps> = ({ accessToken }) => {
-
-    const [message, setMessage] = useState<string>('');
+const MessagesInput: React.FC<MessagesInputProps> = ({ containerRef, secondContainerRef }) => {
+    const [message, setMessage] = useState<string>("");
     const { session } = useAuthentication();
     const { activeConvo } = useConversation();
     const socket = useWebSocket();
     const { setMessages } = useMessages();
 
-
     const messageOnChangeHandler = (e: any) => {
         setMessage(e.target.value);
-    }
-
+    };
 
     const sendMessage = async (e: any) => {
         e.preventDefault();
-        socket.emit('sendMessage', JSON.stringify({
-            userOne: session?.userId,
-            userTwo: activeConvo.toId,
-            message,
-            conversationId: activeConvo.conversationId,
-        }));
-        setMessage('');
-    }
-
+        socket.emit(
+            "sendMessage",
+            JSON.stringify({
+                userOne: session?.userId,
+                userTwo: activeConvo.toId,
+                message,
+                conversationId: activeConvo.conversationId,
+            })
+        );
+        setMessage("");
+    };
 
     useEffect(() => {
-        socket.on('messageSent', (payload) => {
-            if (typeof setMessages === 'function') {
-                setMessages(prev => [...prev, JSON.parse(payload)]);
-            }
-        })
-    }, [setMessages, socket]);
+        socket.on("messageSent", (payload) => {
+            if (typeof setMessages === "function") {
+                setMessages((prev) => [...prev, JSON.parse(payload)]);
+                
+                if (containerRef.current && secondContainerRef.current) {
+                    containerRef.current.scrollTo(0, secondContainerRef.current.offsetHeight);
+                }
 
+            }
+        });
+    }, [setMessages, socket]);
 
     return (
         <div className={styles.container}>
             <div>
-                <FontAwesomeIcon 
-                    icon={faCirclePlus}
-                />
+                <FontAwesomeIcon icon={faCirclePlus} />
             </div>
             <form onSubmit={sendMessage}>
-                <textarea 
+                <textarea
                     value={message}
                     onChange={messageOnChangeHandler}
-                    className='form-control'
+                    className="form-control"
                     required={true}
-                    placeholder='Message....'
+                    placeholder="Message...."
                 />
-                <button 
-                    type='submit' 
-                    className='main-button'
+                <button
+                    type="submit"
+                    className="main-button"
                     onSubmit={sendMessage}
                 >
                     Send
@@ -78,8 +75,6 @@ const MessagesInput : React.FC<MessagesInputProps> = ({ accessToken }) => {
             </form>
         </div>
     );
-}
-
-
+};
 
 export default MessagesInput;
--- src/components/Messages/index.tsx
+++ src/components/Messages/index.tsx
@@ -1,49 +1,56 @@
+import React, { useEffect, useRef } from "react";
+import { useConversation } from "../../custom-hooks/useConversation";
+import { useMessages } from "../../custom-hooks/useMessages";
+import MessagesHeader from "./Header";
+import MessagesInput from "./Input";
+import Message from "./Message";
 
-import React, { useEffect } from 'react';
-import { useConversation } from '../../custom-hooks/useConversation';
-import { useMessages } from '../../custom-hooks/useMessages';
-import MessagesHeader from './Header';
-import MessagesInput from './Input';
-import Message from './Message';
-
-
-
-import styles from './Messages.module.css';
-
+import styles from "./Messages.module.css";
 
 interface MessagesProps {
     accessToken: string;
 }
 
-
-const Messages : React.FC<MessagesProps> = ({ accessToken }) => {
-
+const Messages: React.FC<MessagesProps> = ({ accessToken }) => {
     const { messages } = useMessages();
     const { activeConvo } = useConversation();
-
+    const containerRef = useRef<HTMLDivElement>(null);
+    const secondContainerRef = useRef<HTMLDivElement>(null);
 
     return (
         <>
-            {
-                !activeConvo && messages.length === 0 ? 
+            {!activeConvo && messages.length === 0 ? (
                 <div className={styles.container}>Loading....</div>
-                :
+            ) : (
                 <div className={styles.container}>
                     <MessagesHeader />
-                    <div className={styles.messagesContainer}>
-                        {
-                            messages && messages.map((message: any) => {
-                                return <Message key={message.messageId} message={message}/>
-                            })
-                        }
+                    <div
+                        className={styles.messagesContainer}
+                        ref={containerRef}
+                    >
+                        <div ref={secondContainerRef}>
+                            {
+                                messages &&
+                                messages.map((message: any) => {
+                                    return (
+                                        <Message
+                                            key={message.messageId}
+                                            message={message}
+                                        />
+                                    );
+                                })
+                            }
+                        </div>
                     </div>
-                    <MessagesInput accessToken={accessToken}/>
+                    <MessagesInput 
+                        accessToken={accessToken}  
+                        containerRef={containerRef}
+                        secondContainerRef={secondContainerRef}
+                    />
                 </div>
-            }
+            )}
         </>
-    )
-}
-
-
+    );
+};
 
 export default Messages;
