60cd96cfd356e8c34cfa2344843e7db03fe1cc17
Francis James Tolentino <francistolentino1107@gmail.com>
2022-05-27 23:25:46 +0800

    Service type refactored - removed priceInitial priceType, priceSubType, and priceFinal

--- src/components/Provider/Services/Service/index.tsx
+++ src/components/Provider/Services/Service/index.tsx
@@ -1,4 +1,3 @@
-
 /*
 
 Multi Service Platform - Provider Service Component in Services Content Section
@@ -8,37 +7,19 @@ Author: Tolentino, Francis James S.
 
 */
 
+import styles from "./Service.module.css";
 
-
-import styles from './Service.module.css';
-
-
-
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {
-    faEllipsis
-} from '@fortawesome/free-solid-svg-icons';
-
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEllipsis } from "@fortawesome/free-solid-svg-icons";
 
 // import Image from 'next/image';
 
+import React, { useState } from "react";
+import Link from "next/link";
 
-
-import React, { useState } from 'react';
-import Link from 'next/link';
-
-
-
-import { __backend__ } from '../../../../constants';
-import ServicePopup from './Popup';
-import { Service } from '../../../../../types';
-
-
-
-
-
-
+import { __backend__ } from "../../../../constants";
+import ServicePopup from "./Popup";
+import { Service } from "../../../../../types";
 
 interface ServiceProps {
     service: Service;
@@ -46,48 +27,52 @@ interface ServiceProps {
     accessToken: string;
 }
 
-
-
-const Service: React.FC<ServiceProps> = ({ service, setServices, accessToken}) => {
-
-
+const Service: React.FC<ServiceProps> = ({
+    service,
+    setServices,
+    accessToken,
+}) => {
     const [showPopup, setShowPopup] = useState<boolean>(false);
-
 
     const openPopup = () => setShowPopup(true);
     const closePopup = () => setShowPopup(false);
 
-
     return (
-        <div 
+        <div
             className={styles.serviceContainer}
-            style={service.status === 'active' ? {
-                borderTop: '5px solid var(--mainPurple)',
-            } : {
-                borderTop: '5px solid var(--errorRed)'
-            }}
+            style={
+                service.status === "active"
+                    ? {
+                          borderTop: "5px solid var(--mainPurple)",
+                      }
+                    : {
+                          borderTop: "5px solid var(--errorRed)",
+                      }
+            }
         >
             <div className={styles.serviceTripleDotContainer}>
-                <FontAwesomeIcon 
+                <FontAwesomeIcon
                     icon={faEllipsis}
                     style={{
-                        fontSize: '1.5em',
-                        color: 'var(--white)'
+                        fontSize: "1.5em",
+                        color: "var(--white)",
                     }}
                     onClick={openPopup}
                 />
 
-                {
-                    showPopup && <ServicePopup 
-                                    service={service}
-                                    closePopup={closePopup}
-                                    accessToken={accessToken}
-                                    setServices={setServices}
-                                />
-                }
-
+                {showPopup && (
+                    <ServicePopup
+                        service={service}
+                        closePopup={closePopup}
+                        accessToken={accessToken}
+                        setServices={setServices}
+                    />
+                )}
             </div>
-            <Link href={`/provider/services/${service.serviceId}`} passHref={true}>
+            <Link
+                href={`/provider/services/${service.serviceId}`}
+                passHref={true}
+            >
                 <div>
                     <div className={styles.serviceImageContainer}>
                         {/* <Image 
@@ -101,16 +86,14 @@ const Service: React.FC<ServiceProps> = ({ service, setServices, accessToken}) =
 
                     <div className={styles.serviceTextContainer}>
                         <p>{service.title}</p>
-                        <p className='main-purple-text'>
-                            {service.priceType === 'Range' ? `Starting at P${service.priceInitial}` : `Price: P${service.priceInitial}`}
+                        <p className="main-purple-text">
+                            {`Price: P${service.price}`}
                         </p>
                     </div>
                 </div>
             </Link>
         </div>
-    )
-}
-
-
+    );
+};
 
 export default Service;
--- types.tsx
+++ types.tsx
@@ -1,5 +1,3 @@
-
-
 export type User = {
     userId: string;
     accessToken: string;
@@ -19,15 +17,14 @@ export type User = {
 
     firstLogin: boolean;
     firstProviderLogin: boolean;
-}
-
+};
 
 export type Provider = {
     userId: number;
     accessToken: string;
     username: string | null;
     email: string | null;
-    image: string | null; 
+    image: string | null;
     cover: string | null;
     address: string | null;
     contact: string | null;
@@ -45,8 +42,7 @@ export type Provider = {
 
     Services?: Service[];
     Images: Image[];
-}
-
+};
 
 export type Image = {
     imageId: number;
@@ -55,52 +51,47 @@ export type Image = {
     serviceId: number | null;
     name: string;
     path: string;
-}
+};
 
-
-export type ServiceStatus = 'active' | 'inactive';
-
+export type ServiceStatus = "active" | "inactive";
 
 export type Service = {
-    serviceId: number, 
-    userId: string, 
-    title: string, 
-    serviceDetails: string, 
-    status: ServiceStatus,
-    priceType: 'Flat Rate' | 'Range',
-    priceSubType: 'Per Pax' | 'Per Service',
-    priceInitial: number, 
-    priceFinal: number, 
-    unavailableDates: string, 
-    dislikes: number, 
-    likes: number, 
-    ratings: number, 
-    category: string,
-    Users?: User,
-}
+    serviceId: number;
+    userId: string;
+    title: string;
+    serviceDetails: string;
+    status: ServiceStatus;
+    price: number;
+    unavailableDates: string;
+    dislikes: number;
+    likes: number;
+    ratings: number;
+    category: string;
+    Users?: User;
+};
 
-
-
-
-
-export type BookedServicesFilter = 'To be Approved' | 'To be Rendered' | 'To be Rated' | 'Cancelled' | 'All' | 'Completed'; 
-
+export type BookedServicesFilter =
+    | "To be Approved"
+    | "To be Rendered"
+    | "To be Rated"
+    | "Cancelled"
+    | "All"
+    | "Completed";
 
 export type Booking = {
-    bookId: number, 
-    userId: string, 
-    serviceProviderId: string, 
-    serviceId: number, 
-    pax: number, 
-    price: number, 
-    finalPrice: number,
-    paymentMethod: 'Cash on Delivery' | 'E-Wallet' | 'GCash',
-    paid: boolean,
-    date: string, 
-    status: BookedServicesFilter,
+    bookId: number;
+    userId: string;
+    serviceProviderId: string;
+    serviceId: number;
+    pax: number;
+    price: number;
+    finalPrice: number;
+    paymentMethod: "Cash on Delivery" | "E-Wallet" | "GCash";
+    paid: boolean;
+    date: string;
+    status: BookedServicesFilter;
     cancelReason?: string;
-    Users: User,
-    Service: Service,
-    ServiceProviders: User,
-}
-
+    Users: User;
+    Service: Service;
+    ServiceProviders: User;
+};
