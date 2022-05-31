6a6fcccee4ff64386904ebce65ed150acbbd0d75
Francis James Tolentino <francistolentino1107@gmail.com>
2022-05-31 14:23:26 +0800

    authentication middleware

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -1,14 +1,10 @@
 import { NextRequest, NextResponse } from "next/server";
 
-
-
 export const middleware = (req: NextRequest) => {
     const path = req.nextUrl.pathname;
-
     const accessToken = req.cookies.accessToken;
 
+    if ((path === "/login" && accessToken) || (path === "/register" && accessToken)) return NextResponse.redirect(new URL("/", req.url));
 
-    if (accessToken && (path === '/login' || path === 'register')) {
-        return NextResponse.redirect(new URL('/', req.url));
-    }
-}
+    return NextResponse.next()
+};
