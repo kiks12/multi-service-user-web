fec0afe92084f604d4545b4b963decd518df2bf3
Francis James Tolentino <francistolentino1107@gmail.com>
2022-06-01 11:32:31 +0800

    removed animation

--- src/components/Modals/Modal.tsx
+++ src/components/Modals/Modal.tsx
@@ -1,27 +1,18 @@
 
 
 
-import { AnimatePresence, motion } from 'framer-motion';
+//import { AnimatePresence, motion } from 'framer-motion';
 import React from 'react';
 
 
 
 const Modal : React.FC = ({children}) => {
     return (
-        <AnimatePresence>
-            <motion.div
-                animate={{
-                    scale: [30, 1],
-                    opacity: [0, 0, 0, 1],
-                }}
-                transition={{
-                    duration: 0.2,
-                }}
-                className='modal-container'
-            >
-                {children}
-            </motion.div>
-        </AnimatePresence>
+        <div
+            className='modal-container'
+        >
+            {children}
+        </div>
     )
 }
 
